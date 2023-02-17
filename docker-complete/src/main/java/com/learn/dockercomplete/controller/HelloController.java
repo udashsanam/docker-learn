02d03da213d12e3aed9912c1bf1800ba17f62b7c
udashsanam <udashsanam@gmail.com>
2023-02-17 17:03:20 +0545

    add: docker file:

---
+++ docker-complete/src/main/java/com/learn/dockercomplete/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.learn.dockercomplete.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHelloMessage(){
+
+
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<h1> Learning docker </h1>\n" +
+                "</body>\n" +
+                "</html>";
+
+    }
+}
