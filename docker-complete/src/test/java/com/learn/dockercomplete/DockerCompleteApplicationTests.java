6e9c741b15446bd4d291c603f0c0c356f3211338
udashsanam <udashsanam@gmail.com>
2023-02-17 16:38:41 +0545

    add: initial project for deploy ment

---
+++ docker-complete/src/test/java/com/learn/dockercomplete/DockerCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.dockercomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
