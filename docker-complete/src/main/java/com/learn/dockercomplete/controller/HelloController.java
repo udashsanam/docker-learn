ed748125f013fffe6d7863287cc3bb9343f296e9
udashsanam <udashsanam@gmail.com>
2023-02-24 15:57:50 +0545

    add: file creation and read

--- docker-complete/src/main/java/com/learn/dockercomplete/controller/HelloController.java
+++ docker-complete/src/main/java/com/learn/dockercomplete/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.learn.dockercomplete.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learn.dockercomplete.pojo.FileCreatePojo;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String getHelloMessage(){
+    public String getHelloMessage() {
 
 
         return "<!DOCTYPE html>\n" +
@@ -23,4 +28,54 @@ public class HelloController {
                 "</html>";
 
     }
+
+    @PostMapping("/saveFile")
+    public String saveFile(@RequestBody FileCreatePojo fileCreatePojo) {
+        try {
+
+            Path path = Paths.get("/home/file");
+            if (!Files.exists(path)) {
+                //java.nio.file.Files;
+                Files.createDirectories(path);
+                System.out.println("Directory is created!");
+            }
+
+        } catch (IOException e) {
+
+            System.err.println("Failed to create directory!" + e.getMessage());
+
+        }
+        try {
+            FileWriter myWriter = new FileWriter(fileCreatePojo.getFileName() + ".txt");
+            myWriter.write(fileCreatePojo.getContent());
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return "Successfully created file ";
+    }
+
+    @GetMapping("/readFile")
+    public String readFile(@RequestParam("filename") String filename) throws IOException {
+        // Passing the path to the file as a parameter
+        FileReader fr = new FileReader(filename+".txt");
+        String result = "";
+
+        // Declaring loop variable
+        int i;
+        // Holds true till there is nothing to read
+        while ((i = fr.read()) != -1){
+            // Print all the content of a file
+            System.out.print((char)i);
+            result += (char)i;
+        }
+
+        return result;
+
+
+    }
+
 }
